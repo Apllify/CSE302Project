20f019c8123bd1a1ce845ec0f9b9476a961d24d9
Apllify <ralilahlou50@gmail.com>
2023-09-07 14:52:49 +0200

    feat : done with json to ast

--- lab1/ast2tac.py
+++ lab1/ast2tac.py
@@ -1,11 +1,20 @@
 import sys 
 import json
 
+"""
+FLAWS that I should eventually fix : 
+- Call expression only handles one expression at the moment
+"""
+
 
 ###ALL of the helper classes that we'll use to represent the ACT content
 class Root : 
     def __init__(self, procedure):
         self.procedure = procedure
+
+    def __str__(self):
+        return ("\n \nRoot : \n" + str(self.procedure) )
+
 
 class Procedure: 
     def __init__(self, name, arguments, returntype, body):
@@ -13,6 +22,14 @@ class Procedure:
         self.arguments = arguments
         self.returntype = returntype
         self.body = body
+
+    def __str__(self):
+        child_outputs = []
+        for command in self.body : 
+            child_outputs.append(str(command))
+
+        output = "Procedure lines : \n" + "\n".join(child_outputs)
+        return output
 
         
 
@@ -25,14 +42,23 @@ class StatementVarDecl(Statement):
         self.type = type
         self.init = init
 
+    def __str__(self):
+        return f"Declaring {self.name}"
+
 class StatementAssign(Statement):
     def __init__(self, lvalue, rvalue):
         self.lvalue = lvalue
         self.rvalue = rvalue
 
+    def __str__(self):
+        return f"{str(self.lvalue)} = {str(self.rvalue)}"
+
 class StatementEval(Statement):
     def __init__(self, expression):
         self.expression = expression
+
+    def __str__(self):
+        return str(self.expression)
 
 
 
@@ -45,18 +71,30 @@ class ExpressionCall(Expression):
         self.target = target
         self.argument = argument
 
+    def __str__(self):
+        return f"Calling {self.target} with argument : {self.argument}"
+
 class ExpressionVar(Expression):
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        return self.name
 
 class ExpressionInt(Expression):
     def __init__(self, value):
         self.value = value
 
+    def __str__(self):
+        return str(self.value)
+
 class ExpressionUniOp(Expression):
     def __init__(self, operator, argument):
         self.operator = operator
         self.argument = argument
+
+    def __str__(self):
+        return f"({self.operator} {str(self.argument)})"
 
 class ExpressionBinOp(Expression):
     def __init__(self, operator, left, right ):
@@ -64,11 +102,93 @@ class ExpressionBinOp(Expression):
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return f"{str(self.left)} {self.operator} {str(self.right)}"
 
 
 
-def json_to_AST(json_obj):
-    pass
+def json_to_name(js_obj):
+    return js_obj[1]['value']
+
+def json_to_type(js_obj):
+    return js_obj[0]
+
+
+
+
+def json_to_AST(json_obj, is_root = False):
+    
+
+    #the root object is special and doesn't have a name
+    if (is_root):
+        return Root(json_to_AST(json_obj["ast"][0]))
+
+    #otherwise cycle through the names
+    obj_type = json_obj[0]
+
+    print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa")
+    print(obj_type)
+    if obj_type == "<decl:proc>":
+
+        name = json_to_name(json_obj[1]["name"])
+        arguments = json_obj[1]["arguments"]
+        return_type = json_obj[1]["returntype"]
+        body = json_obj[1]["body"]
+
+        #parse the contents of body
+        parsed_body = []
+        for command in body:
+            parsed_body.append(json_to_AST(command)) 
+
+        return Procedure(name, arguments, return_type, parsed_body)
+
+    elif obj_type == "<statement:vardecl>":
+        name = json_to_name(json_obj[1]["name"])
+        type = json_to_type(json_obj[1]["type"])
+        init = json_to_AST(json_obj[1]["init"])
+
+        return StatementVarDecl(name, type, init)
+
+    elif obj_type == "<statement:assign>":
+        lvalue = json_to_name(json_obj[1]["lvalue"][1]["name"])
+        rvalue = json_to_AST(json_obj[1]["rvalue"])
+
+        return StatementAssign(lvalue, rvalue)
+
+    elif obj_type == "<statement:eval>":
+
+        expression = json_to_AST(json_obj[1]["expression"])
+
+        return StatementEval(expression)
+
+    elif obj_type == "<expression:call>":
+        target = json_to_name(json_obj[1]["target"])
+        argument = json_to_AST(json_obj[1]["arguments"][0])
+
+        return ExpressionCall(target, argument)
+
+
+    elif obj_type == '<expression:var>':
+        return ExpressionVar(json_to_name(json_obj[1]['name']))
+
+    elif obj_type == '<expression:int>':
+        return ExpressionInt(json_obj[1]['value'])
+        
+    elif obj_type == '<expression:uniop>':
+        operator = json_to_name(json_obj[1]['operator'])
+        argument = json_to_AST(json_obj[1]['argument']) 
+        return ExpressionUniOp(operator, argument)
+
+    elif obj_type == '<expression:binop>':
+        operator = json_to_name(json_obj[1]["operator"])
+        left = json_to_AST(json_obj[1]["left"])
+        right = json_to_AST(json_obj[1]["right"])
+        return ExpressionBinOp(operator, left, right)
+
+    else:
+
+        #if no option was identified, raise an error
+        raise ValueError(f'Unrecognized <expression>: {json_obj[0]}')
 
 
 if __name__ == "__main__" : 
@@ -76,8 +196,8 @@ if __name__ == "__main__" :
     cmd_inputs = sys.argv
     filepath_index = 1
 
-    ast_file_path = ""
-    ast_json_object= None
+    json_file_path = ""
+    json_object = None
     flag = "tmm"
 
 
@@ -101,18 +221,21 @@ if __name__ == "__main__" :
 
         
     #now finally load the ast json file
-    ast_file_path = cmd_inputs[filepath_index]
+    json_file_path = cmd_inputs[filepath_index]
     try : 
-        with open(ast_file_path, "r") as ast_file:
-            ast_json_object = json.load(ast_file)
+        with open(json_file_path, "r") as ast_file:
+            json_object = json.load(ast_file)
     except: 
         raise Exception("File Error : Provided Json file could not be loaded.")
 
 
+    #convert the json file to a better format
+    ast_object = json_to_AST(json_object, True)
+    print(str(ast_object))
 
     #this should be at the verrrry end of the conversion
-    print("\n \n-----------------DONE-----------------")
-    print("Conversion succesful ! \n \n")
+    print("\n \nConversion succesful !")
+    print("-----------------DONE----------------- \n \n")
 
 
 
