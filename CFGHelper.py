9cfd3d8015df689fd9c02e772d69b3c6268b5c7a
Apllify <ralilahlou50@gmail.com>
2023-10-26 13:14:39 +0200

    Slight progress

--- CFGHelper.py
+++ CFGHelper.py
@@ -1,12 +1,12 @@
 class Jump :
 
-    jump_types = [
-        "jmp",
-
+    conditional_jumps = [
         "jl", "jle",
         "jnl", "jnle",
         "jz", "jnz"
     ]
+
+    # normal jump : jmp
 
     def __init__(self, jump_type, args = []):
 
@@ -85,52 +85,82 @@ def TAC_command(opcode, args, result_reg=-1):
     return entry
 
 
-def TAC2Blocks(TAC_dict):
+class TAC2Blocks:
     """
     Assumes the only procedure is the main procedure
     input : the dictionary form of a tac program
     output : a list of block type elements 
     """
-    body = TAC_dict[0]["body"]
 
-    #find/add entry label if applicable
-    if body[0]["opcode"] != "label":
-        entry_label = TAC_command("label", ["%.Lentry"], -1)
-    else : 
-        entry_label = body[0]
-        body = body[1:]
+    def __init__(self, TAC_dict):
+        self.TAC_dict = TAC_dict
+        self.body = TAC_dict[0]["body"]
+        self.reserved_labels=  TAC_dict[0]["labels"]
 
-    #start by adding all the blocks WITHOUT the edges
-    all_blocks = []
-    cur_block = Block(entry_label, [])
-    i=0 
+        self.label_count = 0
 
-    while i < len(body) :
-        command = body[i]
-        opcode = command["opcode"]
+        self.all_blocks = []
 
-        if opcode in Jump.jump_types:
-            #eat up all successor jump commands, then finish block
-            cur_block.content.append(command)
 
-            while i < len(body):
-                next_command = body[i+1]
-            
-            
+    #new label, prefixed with 
+    def new_label(self):
+        """
+        Returns fresh new label of the form %.La[number]
+        TODO : For now assumes that all tac label names only contain numerics
+        """
+        label_name = f"%.LA{self.label_count}"
+        self.label_count += 1
+        return label_name
+        
 
-        elif opcode == "ret":
-            #end the conversion
-            cur_block.content.append(command)
-            all_blocks.append(cur_block)
-            break
 
-        elif opcode == "label":
-            #do something
-            pass
+
+    def convert(self):
+        #find/add entry label if applicable
+        if self.body[0]["opcode"] != "label":
+            entry_label = TAC_command("label", [self.new_label()], -1)
         else : 
-            cur_block.content.append(command)
+            entry_label = self.body[0]
+            self.body = self.body[1:]
 
-        i += 1
+        #start by adding all the blocks WITHOUT the edges
+        cur_block = Block(entry_label, [])
+        i=0 
+
+        while i < len(self.body) :
+            command = self.body[i]
+            opcode = command["opcode"]
+
+            if opcode in Jump.conditional_jumps:
+                #if the last command in block was also a cond jump just add
+                if (True):
+                    cur_block.content.append(command)
+                else: 
+                    #otherwise create new block with new label and add ourselves to it
+                    self.all_blocks.append(cur_block)
+                    cur_block = CFG([], dict()) 
+                    cur_block.content.append(TAC_command("label", [self.new_label()], -1))
+                    cur_block.content.append(command)
+
+
+
+            elif opcode == "jmp":
+
+                
+
+            elif opcode == "ret":
+                #end the conversion
+                cur_block.content.append(command)
+                all_blocks.append(cur_block)
+                break
+
+            elif opcode == "label":
+                #do something
+                pass
+            else : 
+                cur_block.content.append(command)
+
+            i += 1
 
 
 
